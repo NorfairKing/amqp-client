623bff4b9f750662222f41980e49e90e38573987
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-25 17:11:08 +0100

    reading and rendering frames

--- amqp-serialisation/src/AMQP/Serialisation.hs
+++ amqp-serialisation/src/AMQP/Serialisation.hs
@@ -5,7 +5,9 @@
 
 module AMQP.Serialisation where
 
-import Data.Attoparsec.ByteString
+import Control.Monad
+import Data.Attoparsec.Binary as Parse
+import Data.Attoparsec.ByteString as Parse
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as SB
 import Data.ByteString.Builder as ByteString (Builder)
@@ -25,6 +27,8 @@ data FrameType
   | HeartbeatFrame
   deriving (Show, Eq, Generic, Enum, Bounded)
 
+instance Validity FrameType
+
 -- TODO get these from the spec
 buildFrameType :: FrameType -> ByteString.Builder
 buildFrameType = \case
@@ -32,9 +36,19 @@ buildFrameType = \case
   HeaderFrame -> SBB.word8 2
   BodyFrame -> SBB.word8 3
   -- QUESTION: The pdf says 4 but the spec says 8, which is it?
+  -- ANSWER: We'll go with what the spec says.
   HeartbeatFrame -> SBB.word8 8
 
-instance Validity FrameType
+parseFrameType :: Parser FrameType
+parseFrameType = do
+  w <- Parse.anyWord8
+  -- TODO get these from the spec
+  case w of
+    1 -> pure MethodFrame
+    2 -> pure HeaderFrame
+    3 -> pure BodyFrame
+    8 -> pure HeartbeatFrame
+    _ -> fail $ "Unknown frame type: " <> show w
 
 data Frame = Frame
   { frameType :: !FrameType,
@@ -49,12 +63,26 @@ buildFrame :: Frame -> ByteString.Builder
 buildFrame Frame {..} =
   mconcat
     [ buildFrameType frameType,
-      SBB.word16LE frameChannel, -- QUESTION: Should this be little-endian?
+      -- QUESTION: Should this be little-endian?
+      -- ANSWER: No, the spec says that it must be network byte order, or big-endian.
+      SBB.word16BE frameChannel,
+      -- QUESTION: Should this be little-endian?
+      -- ANSWER: the spec says that it must be network byte order, or big-endian.
+      --
       -- The 'fromIntegral' should be safe because we cast from Int to Word32.
-      SBB.word32LE (fromIntegral (SB.length framePayload)), -- QUESTION: Should this be little-endian?
+      SBB.word32BE (fromIntegral (SB.length framePayload)),
       SBB.byteString framePayload,
       SBB.word8 frameEnd
     ]
 
 frameEnd :: Word8
 frameEnd = 206 -- TODO get this from the spec
+
+parseFrame :: Parser Frame
+parseFrame = do
+  frameType <- parseFrameType
+  frameChannel <- anyWord16be
+  frameLength <- anyWord32be
+  framePayload <- Parse.take (fromIntegral frameLength)
+  void $ Parse.word8 frameEnd
+  pure Frame {..}
--- amqp-serialisation/test/AMQP/SerialisationSpec.hs
+++ amqp-serialisation/test/AMQP/SerialisationSpec.hs
@@ -2,12 +2,24 @@ module AMQP.SerialisationSpec (spec) where
 
 import AMQP.Serialisation
 import Control.Monad
+import Data.Attoparsec.ByteString
 import qualified Data.ByteString as SB
 import Data.ByteString.Builder as ByteString (Builder)
 import qualified Data.ByteString.Builder as SBB
 import qualified Data.ByteString.Lazy as LB
 import Data.Char as Char
+import Data.GenValidity
+import Data.GenValidity.ByteString ()
 import Test.Syd
+import Test.Syd.Validity
+
+instance GenValid FrameType where
+  genValid = genValidStructurallyWithoutExtraChecking
+  shrinkValid = shrinkValidStructurallyWithoutExtraFiltering
+
+instance GenValid Frame where
+  genValid = genValidStructurallyWithoutExtraChecking
+  shrinkValid = shrinkValidStructurallyWithoutExtraFiltering
 
 spec :: Spec
 spec = do
@@ -19,3 +31,13 @@ spec = do
     forM_ [minBound .. maxBound] $ \ft ->
       it (unwords ["renders the", show ft, "frame type the same as before"]) $
         pureGoldenByteStringBuilderFile ("test_resources/frame-type/" <> map Char.toLower (show ft)) (buildFrameType ft)
+
+  describe "parseFrameType" $
+    it "can parse whatever 'buildFrameType' builds'" $
+      forAllValid $ \ft ->
+        parseOnly parseFrameType (LB.toStrict (SBB.toLazyByteString (buildFrameType ft))) `shouldBe` Right ft
+
+  describe "parseFrame" $
+    it "can parse whatever 'buildFrame' builds'" $
+      forAllValid $ \ft ->
+        parseOnly parseFrame (LB.toStrict (SBB.toLazyByteString (buildFrame ft))) `shouldBe` Right ft
