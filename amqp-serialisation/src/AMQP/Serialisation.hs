9cab290d79bb004eb21b8b9ddb87e290babdeaf3
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-25 16:45:05 +0100

    frame types

--- amqp-serialisation/src/AMQP/Serialisation.hs
+++ amqp-serialisation/src/AMQP/Serialisation.hs
@@ -1,32 +1,57 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module AMQP.Serialisation where
 
+import Data.Attoparsec.ByteString
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as SB
 import Data.ByteString.Builder as ByteString (Builder)
 import qualified Data.ByteString.Builder as SBB
+import Data.Validity
+import Data.Validity.ByteString
 import Data.Word
 import GHC.Generics (Generic)
 
 protocolHeader :: ByteString.Builder
 protocolHeader = "AMQP0091"
 
+data FrameType
+  = MethodFrame
+  | HeaderFrame
+  | BodyFrame
+  | HeartbeatFrame
+  deriving (Show, Eq, Generic, Enum, Bounded)
+
+-- TODO get these from the spec
+buildFrameType :: FrameType -> ByteString.Builder
+buildFrameType = \case
+  MethodFrame -> SBB.word8 1
+  HeaderFrame -> SBB.word8 2
+  BodyFrame -> SBB.word8 3
+  -- QUESTION: The pdf says 4 but the spec says 8, which is it?
+  HeartbeatFrame -> SBB.word8 8
+
+instance Validity FrameType
+
 data Frame = Frame
-  { frameType :: !Word8,
+  { frameType :: !FrameType,
     frameChannel :: !Word16,
-    frameSize :: !Word32,
     framePayload :: !ByteString
   }
   deriving (Show, Eq, Generic)
 
+instance Validity Frame
+
 buildFrame :: Frame -> ByteString.Builder
 buildFrame Frame {..} =
   mconcat
-    [ SBB.word8 frameType,
+    [ buildFrameType frameType,
       SBB.word16LE frameChannel, -- QUESTION: Should this be little-endian?
-      SBB.word32LE frameSize, -- QUESTION: Should this be little-endian?
+      -- The 'fromIntegral' should be safe because we cast from Int to Word32.
+      SBB.word32LE (fromIntegral (SB.length framePayload)), -- QUESTION: Should this be little-endian?
       SBB.byteString framePayload,
       SBB.word8 frameEnd
     ]
--- amqp-serialisation/test/AMQP/SerialisationSpec.hs
+++ amqp-serialisation/test/AMQP/SerialisationSpec.hs
@@ -1,10 +1,12 @@
 module AMQP.SerialisationSpec (spec) where
 
 import AMQP.Serialisation
+import Control.Monad
 import qualified Data.ByteString as SB
 import Data.ByteString.Builder as ByteString (Builder)
 import qualified Data.ByteString.Builder as SBB
 import qualified Data.ByteString.Lazy as LB
+import Data.Char as Char
 import Test.Syd
 
 spec :: Spec
@@ -12,3 +14,8 @@ spec = do
   describe "protocolHeader" $ do
     it "is 8 octets long" $ LB.length (SBB.toLazyByteString protocolHeader) `shouldBe` 8
     it "stays the same" $ pureGoldenByteStringBuilderFile "test_resources/protocol-header.dat" protocolHeader
+
+  describe "buildFrameType" $
+    forM_ [minBound .. maxBound] $ \ft ->
+      it (unwords ["renders the", show ft, "frame type the same as before"]) $
+        pureGoldenByteStringBuilderFile ("test_resources/frame-type/" <> map Char.toLower (show ft)) (buildFrameType ft)
