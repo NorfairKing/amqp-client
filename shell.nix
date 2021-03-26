ac915d89499fb2d524a1651a1af6b81ca370b027
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-26 16:41:43 +0100

    pre-commit-hooks

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
 let
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
-in pkgs.mkShell {
+  pkgs = import sources.nixpkgs { };
+  pre-commit = import ./nix/pre-commit.nix;
+in
+pkgs.mkShell {
   name = "amqp-shell";
   buildInputs = with pkgs; [
     (import sources.niv { inherit pkgs; }).niv
   ];
+  shellHook = ''
+    ${pre-commit.check.shellHook}
+  '';
 }
