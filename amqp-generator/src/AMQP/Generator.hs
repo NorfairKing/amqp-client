6374dcc3d07a2af4e29d69ea6cf7b3aeee9e5183
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-26 16:39:42 +0100

    Generating constants and domain types

--- amqp-generator/src/AMQP/Generator.hs
+++ amqp-generator/src/AMQP/Generator.hs
@@ -12,6 +12,7 @@ import AMQP.Generator.Parse as AMQP hiding (Doc (..))
 import qualified AMQP.Generator.Parse as AMQP
 import Control.Monad
 import Data.Either (partitionEithers)
+import Data.List
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe
@@ -39,11 +40,21 @@ main = do
 generateFrom :: FilePath -> IO ()
 generateFrom fp = do
   spec <- AMQP.parseSpecFromFile fp
-  pPrint spec
-  putStrLn $ render $ to_HPJ_Doc $ genConstantsDoc $ amqpSpecConstants spec
+  pPrint $ amqpSpecDomainTypes spec
+  let moduleString = render $ to_HPJ_Doc $ genGeneratedModule spec
+  writeFile "amqp-serialisation/src/AMQP/Serialisation/Generated.hs" moduleString
+
+genGeneratedModule :: AMQP.AMQPSpec -> Doc
+genGeneratedModule AMQP.AMQPSpec {..} =
+  vcat
+    [ text "module AMQP.Serialisation.Generated where",
+      text "import AMQP.Serialisation.Base",
+      genConstantsDoc amqpSpecConstants,
+      genDomainTypesDoc amqpSpecDomainTypes
+    ]
 
 genConstantsDoc :: [Constant] -> Doc
-genConstantsDoc = vcat . map genConstantDoc
+genConstantsDoc = vcat . intersperse (text "") . map genConstantDoc
 
 genConstantDoc :: Constant -> Doc
 genConstantDoc c@AMQP.Constant {..} =
@@ -54,7 +65,7 @@ genConstantDoc c@AMQP.Constant {..} =
 
 constantDecs :: Constant -> [Dec]
 constantDecs AMQP.Constant {..} =
-  let n = mkHaskellName constantName
+  let n = mkHaskellVarName constantName
    in [ SigD n (ConT (mkName "Word")),
         FunD
           n
@@ -83,5 +94,39 @@ haddockIntro t = text "-- |" <+> text (T.unpack t)
 comment :: Text -> Doc
 comment = vcat . map ((text "--" <+>) . text) . lines . T.unpack
 
-mkHaskellName :: Text -> Name
-mkHaskellName = mkName . toCamel . fromKebab . T.unpack
+mkHaskellVarName :: Text -> Name
+mkHaskellVarName = mkName . toCamel . fromKebab . T.unpack
+
+genDomainTypesDoc :: [DomainType] -> Doc
+genDomainTypesDoc = vcat . intersperse (text "") . mapMaybe genDomainTypeDoc
+
+genDomainTypeDoc :: DomainType -> Maybe Doc
+genDomainTypeDoc dt@AMQP.DomainType {..} =
+  if domainTypeName == domainTypeType
+    then Nothing -- This means it's one of the base types
+    else
+      Just $
+        vcat
+          [ genHaddocks (maybe domainTypeName (\l -> T.concat [domainTypeName, ": ", l]) domainTypeLabel) domainTypeDoc,
+            ppr_list (domainTypeDecs dt)
+          ]
+
+domainTypeDecs :: DomainType -> [Dec]
+domainTypeDecs DomainType {..} =
+  [ TySynD (mkHaskellTypeName domainTypeName) [] (ConT (typeTranslator domainTypeType))
+  ]
+
+typeTranslator :: Text -> Name
+typeTranslator = \case
+  "bit" -> mkName "Bit"
+  "octet" -> mkName "Octet"
+  "short" -> mkName "ShortUInt"
+  "long" -> mkName "LongUInt"
+  "longlong" -> mkName "LongLongUInt"
+  "shortstr" -> mkName "ShortString"
+  "longstr" -> mkName "LongString"
+  "table" -> mkName "FieldTable"
+  t -> mkHaskellTypeName t
+
+mkHaskellTypeName :: Text -> Name
+mkHaskellTypeName = mkName . toPascal . fromKebab . T.unpack
